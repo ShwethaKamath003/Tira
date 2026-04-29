fb2826ff80af854ea1ae44410c699507d2ef9303
Shwetha Kamath <kamathshwetha2003@gmail.com>
2026-04-29 13:31:50 +0530

    fix backend updates

--- server/index.js
+++ server/index.js
@@ -16,7 +16,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/images", express.static("public/Images"));
-
+app.get("/", (req, res) => {
+  res.send("Tira backend is running ");
+});
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("Mongo DB connected"))
